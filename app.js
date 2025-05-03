e7f030c0506acf4f6e237647e7849c002da2bf39
chetanukani <ukanichetan007@gmail.com>
2025-05-03 11:37:23 +0530

    update: port

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-require('dotenv').config()
+require('dotenv').config();
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello World! Node' });
 });
 
-app.listen(process.env.PORT, () => {
-    console.log('server is running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
 });
